ac9961c265cd05a39a4107d6a980f24f1113fcc3
lunyamwis <lutherlunyamwi@gmail.com>
2025-08-19 18:59:06 +0300

    setting up

--- store/models.py
+++ store/models.py
@@ -56,7 +56,7 @@ class Cart(models.Model):
     date_added = models.DateField(auto_now_add=True)
 
     class Meta:
-        db_table = 'cart'
+        db_table = 'Cart'
         ordering = ['date_added']
 
     def __str__(self):
--- store/views.py
+++ store/views.py
@@ -13,11 +13,7 @@ from django.template.loader import get_template
 from django.core.mail import EmailMessage
 
 
-def home(request):
-    return render(request, 'adventproclaimershop.html')
-
-
-def home_two(request, category_slug=None):
+def home(request, category_slug=None):
     category_page = None
     products_list = None
     if category_slug != None:
